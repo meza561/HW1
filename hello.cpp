f197aa33da1e88906c27b5d58d0202734758473c
meza561 <noahmeza561@gmail.com>
2026-09-01 12:28:26 -0400

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+// first c++ program for class
+// \n makes it skip to next line
+#include <iostream>
+using namespace std;
+int main () {
+    cout << "Hello my name is Noah Meza\nMy major is biomathematics\nOne reason i am taking isc3313 is so that i can learn new implementations for code\nIn my free time i like to vibe code apps";
+    return 0;
+}
---
+++ hello2.cpp
@@ -0,0 +1,12 @@
+// first c++ program for class
+// \n makes the output skip to next line
+#include <iostream>
+using namespace std;
+int main () {
+    cout << "Hello my name is Noah Meza\n";
+    cout << "My major is biomathematics\n";
+    cout << "One reason i am taking isc3313 is so that i can learn new implementations for code\n";
+    cout << "In my free time i like to vibe code apps\n";
+    cout << "I want to be a medical technologies researcher in the future";
+    return 0;
+}
