434c130be22890bf4a52e5c846f0a2ca85242bd5
meza561 <noahmeza561@gmail.com>
2026-09-01 12:48:24 -0400

    add new file

---
+++ int.cpp
@@ -0,0 +1,11 @@
+int main;
+    int x,y,xpy,xmy,xsy
+
+    xpy = x+y;
+    xmy = x*y;
+    xsy = x-y;
+
+    cout <<"x = " << X << "               " <<"y = " << y << endl;
+    cout << "x + y = " << xpy << endl;
+    cout << "x * y = " << xmy << endl;
+    cout << "x - y = " << xsy << endl;
