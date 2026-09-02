7158242ba064709d874f89910c14940a5b72f497
meza561 <noahmeza561@gmail.com>
2026-09-02 12:22:39 -0400

    updated int.cpp

--- int.cpp
+++ int.cpp
@@ -1,11 +1,43 @@
-int main;
-    int x,y,xpy,xmy,xsy
+#include <iostream>
+#include <string>
 
-    xpy = x+y;
-    xmy = x*y;
-    xsy = x-y;
+using namespace std;
 
-    cout <<"x = " << X << "               " <<"y = " << y << endl;
-    cout << "x + y = " << xpy << endl;
-    cout << "x * y = " << xmy << endl;
-    cout << "x - y = " << xsy << endl;
+int main() {
+  int x;
+  int y;
+  int xpy,xty,xsy,xdy;
+  char letterA = 'f';
+  string message = "Hello World";
+
+  cout << "Enter a value for x." << endl;
+  cin >> x;
+  cout << "Enter a value for y." << endl;
+  cin >> y;
+
+  xpy = x+y;
+  xty = x*y;
+  xsy = x-y;
+  xdy = x/y;
+
+  cout << "x = " << x << "          " << "y = " << y << endl;
+  cout << "x is less than y " << (x<y) << endl;
+  cout << "x + y = " << xpy << endl;
+  cout << "x * y = " << xty << endl;
+  cout << "x - y = " << xsy << endl;
+  cout << "x / y = " << xdy << endl;
+
+  cout << "x mod y is " << x%y << endl;
+  x++;
+  cout << "One number greater than x is " << x << endl;
+//  int x;
+//  x = 5;
+//   cout << x << endl;
+//   cout << letterA << endl;
+//   cout << message << endl;
+//
+//  message = "I'm OK";
+//  cout << message << endl;
+
+  return 0;
+}
